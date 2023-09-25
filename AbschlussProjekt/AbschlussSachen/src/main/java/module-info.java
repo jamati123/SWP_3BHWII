3d28b942f63399bc81f2d217edd507ef09f90789
jamati123 <fourtyeightcrash@gmail.com>
2023-09-25 11:18:06 +0200

    Wichtiger Commit

--- AbschlussProjekt/AbschlussSachen/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.abschlusssachen {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.abschlusssachen to javafx.fxml;
-    exports com.example.abschlusssachen;
-}
---
+++ ArrayList/TestArrayList.java
@@ -0,0 +1,48 @@
+package ArrayList;
+
+
+
+
+public class TestArrayList {
+    int[] data;
+    int capacity;
+    int index;
+
+    public TestArrayList(int cap) {
+        this.capacity = capacity;
+        this.data = new int[cap];
+        this.capacity = cap;
+        this.index = 0;
+    }
+
+    public boolean add(int value) {
+        if (index < capacity) {
+            data[index] = value;
+            index++;
+
+        } else {
+            System.out.println("Do you wanna double it and give it to the next person?");
+            this.capacity = capacity * 2;
+            int[] temp = new int[capacity];
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[i];
+            }
+            this.data = temp;
+            this.data[index++] = value;
+        }
+
+    return true;
+    }
+
+    public static void main(String[] args) {
+        TestArrayList list = new TestArrayList(5);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+    }
+}
+
